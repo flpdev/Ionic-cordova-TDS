34da35a4c0e6908698ba9e019c189b321448b807
Felipe Moreira <fm.felipe@gmail.com>
2018-11-12 20:34:14 -0300

    aula1 - comit4

--- exemplo2/src/app/app.component.ts
+++ exemplo2/src/app/app.component.ts
@@ -22,7 +22,9 @@ export class MyApp {
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'List', component: ListPage },
+      { title: 'Contato', component: HomePage },
+      { title: 'Felipe', component: ListPage }
     ];
 
   }
