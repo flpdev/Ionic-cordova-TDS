66c959d3b490757928460888bdf1675ec9377d74
Felipe Moreira <fm.felipe@gmail.com>
2018-11-12 21:54:03 -0300

    aula1-comit6

--- lista-tarefas/src/pages/home/home.ts
+++ lista-tarefas/src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
+import { NavController, IonicPage } from 'ionic-angular'; // importar ionic page para lazyload
+@IonicPage() // importar ionic page para lazyload
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
