5a6e6f224f2f49a34b50926abbe957e15f241c16
Felipe Moreira <fm.felipe@gmail.com>
2018-11-12 21:00:36 -0300

    aula1 - comit5

--- exemplo3/src/pages/tabs/tabs.ts
+++ exemplo3/src/pages/tabs/tabs.ts
@@ -2,17 +2,18 @@ import { Component } from '@angular/core';
 
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
+//import { HomePage } from '../home/home';
+//import { BasketPage } from '../basket/basket';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
+  tab1Root = 'HomePage';
   tab2Root = AboutPage;
   tab3Root = ContactPage;
-  tab4Root = HomePage;
+  tab4Root = 'BasketPage';
 
   constructor() {
 
