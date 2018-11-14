7a498b4524422da7ed6b43e3fd164800e7c16444
Felipe Moreira <fm.felipe@gmail.com>
2018-11-14 18:50:56 -0300

    aula2-comit1

--- lista-tarefas/src/pages/home/home.ts
+++ lista-tarefas/src/pages/home/home.ts
@@ -7,8 +7,28 @@ import { NavController, IonicPage } from 'ionic-angular'; // importar ionic page
 })
 export class HomePage {
 
+  //Esse é o nossso objeto tarefa (os campos que constam no htl)
+  tarefa: any=[{
+    nome:"",
+    data:"",
+    cor:""
+  }]
+
+ //Esse é o array de cores, para escolhermos ao cadatrar uma tarefa
+  cores: Array<Array<String>> = [
+    ["Azul", "#0039b5"],
+    ["Verde", "#00b51e"],
+    ["Amarelo", "#c1c100"],
+    ["Vermelho", "#b50000"],
+    ["Preto","#000000"]
+  ]
+
   constructor(public navCtrl: NavController) {
 
+  }
+
+  salvarTarefa(){
+    console.log(this.tarefa)
   }
 
 }
