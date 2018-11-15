fda6c89f27541e1c44ad3e574a633eb7c766fffa
Felipe Moreira <fm.felipe@gmail.com>
2018-11-14 21:45:11 -0300

    aula2-comit5

--- lista-tarefas/src/pages/tarefas/tarefas.ts
+++ lista-tarefas/src/pages/tarefas/tarefas.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController, LoadingController } from 'ionic-angular';
+import { Storage } from '@ionic/storage'; // IMPORT DO STORAGE PARA ACESSAR AS INFORMAÇÕES CADASTRADAS ANTERIORMENTE
 
 /**
  * Generated class for the TarefasPage page.
@@ -15,11 +16,31 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class TarefasPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  tarefasAberto: any;
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad TarefasPage');
-  }
+  constructor(public storage: Storage, public navCtrl: NavController, public toast: ToastController, public loading: LoadingController) {
 
+    let load = this.loading.create({
+      content: 'Carregando tarefas...'
+    });
+
+    load.present();
+
+    this.storage.get('tarefasAberto').then(tarefasRetornadas =>{
+      this.tarefasAberto = []; 
+      for (let index = 0; index < tarefasRetornadas.length; index++) {
+        this.tarefasAberto.push({
+          nome: tarefasRetornadas[index].nome,
+          data: tarefasRetornadas[index].data,
+          cor: tarefasRetornadas[index].cor
+        })   
+      }
+
+      console.log(this.tarefasAberto);
+
+    });
+
+  load.dismiss();
+
+}
 }
