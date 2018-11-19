3b5ea91b9b8758b61416e5c81a01d10a2d3d1a05
Felipe Moreira <fm.felipe@gmail.com>
2018-11-19 15:41:04 -0300

    comit1

--- registracrise/src/pages/home/home.ts
+++ registracrise/src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, IonicPage, ToastController, LoadingController, Toast } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'page-home',
@@ -9,6 +10,7 @@ export class HomePage {
 
   registro: any=[{
     intensidade:"",
+    turno:"",
     data:"",
     observacao:""
   }]
@@ -19,8 +21,75 @@ export class HomePage {
     ["Forte", "#FF0000"]
   ]
 
-  constructor(public navCtrl: NavController) {
+  turno: Array<Array<String>> = [
+    ["Manhã", "#A9D0F5"],
+    ["Tarde", "#5882FA"],
+    ["Noite", "#013ADF"]
+  ]
 
+  todosRegistros: any;
+
+  constructor(public navCtrl: NavController, public storage: Storage, public toast: ToastController, public loading: LoadingController) {
+
+    let load = this.loading.create({
+      content: 'Carregando registros...'
+    });
+
+    load.present();
+
+
+    this.storage.get('registroCrises').then(registrosRetornados =>{
+      this.todosRegistros = [];
+
+      for (let index = 0; index < registrosRetornados.length; index++) {
+        this.todosRegistros.push({
+          nome: registrosRetornados[index].intensidade,
+          turno: registrosRetornados[index].turno,
+          data: registrosRetornados[index].data,
+          observacao: registrosRetornados[index].observacao
+        })        
+      }
+    });
+
+    load.dismiss();
+
+  }
+
+  salvarRegistro(){
+
+    let mensagem = this.toast.create({
+      message: 'Registro inserido',
+      duration: 3000,
+      position: 'bottom'
+    });
+
+    this.todosRegistros.push({
+      intensidade: this.registro.intensidade,
+      turno: this.registro.turno,
+      data: this.registro.data,
+      observacao: this.registro.observacao
+    });
+
+    console.log(this.todosRegistros);
+
+    this.storage.set('registroCrises', this.todosRegistros);
+
+    mensagem.present();
+    this.limparCampos();
+
+  }
+
+  limparCampos(){
+
+    this.registro.intensidade = '';
+    this.registro.turno='';
+    this.registro.data = '';
+    this.registro.observacao = '';
+
+  }
+
+  exibirRegistros(){
+    this.navCtrl.push("RegistrosPage")
   }
 
 }
