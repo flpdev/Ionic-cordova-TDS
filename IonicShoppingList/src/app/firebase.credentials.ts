254f04ef198ef0258b71b415e8520bf18e55023e
Felipe Moreira <fm.felipe@gmail.com>
2018-11-30 17:35:31 -0300

    30-11-18

---
+++ IonicShoppingList/src/app/firebase.credentials.ts
@@ -0,0 +1,13 @@
+
+// ARQUIVO CRIADO COM OS DADOS DE CONEXÃO AO FIREBASE.
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyCiic0J2z338HrWpXtLNXy3PswF1zsXGhc",
+    authDomain: "ionicshoppingdb.firebaseapp.com",
+    databaseURL: "https://ionicshoppingdb.firebaseio.com",
+    projectId: "ionicshoppingdb",
+    storageBucket: "ionicshoppingdb.appspot.com",
+    messagingSenderId: "640791199047"
+  };
+
+  // POSTERIORMENTE INSTALAR O ANGULARFIREBASE2
+  //npm i firebase angularfire2
---
+++ IonicShoppingList/src/pages/home/home.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ShoppingListService } from '../../services/shopping-list/shopping-list.service';
+import { Observable } from 'rxjs';
+import { Item } from '../../models/item/item.model';
+
+/**
+ * Generated class for the HomePage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html',
+})
+export class HomePage {
+
+  shoppingList$ : Observable<Item[]>
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+                     private shopping : ShoppingListService) {
+
+    this.shoppingList$ = this.shopping
+      .getShoppingList() //DB LIST
+      .snapshotChanges() // KEY AND VALUE
+      .map(
+        changes => {
+          return changes.map(c=> ({
+            key: c.payload.key,
+            ...c.payload.val(),
+          }));
+        });
+  }
+}
